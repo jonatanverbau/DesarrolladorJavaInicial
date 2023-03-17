c44cb11db207fb97713983f070a2b44794bd6251
Jonatan <jonatanverbau@gmail.com>
2023-03-17 12:55:19 -0300

    anotaciones

--- Codificador.java
+++ Codificador.java
@@ -14,14 +14,20 @@ public class Codificador {
 
     public static String codificar(String texto, int desplazamiento) {
         StringBuilder resultado = new StringBuilder();
+        //convierte texto a minusculas
         texto = texto.toLowerCase();
+
         for (int i = 0; i < texto.length(); i++) {
             char caracter = texto.charAt(i);
+            //con indexOf ubicamos la posición de la variable caracter dentro de la constante abecedario
             int indice = ABECEDARIO.indexOf(caracter);
+            //en el caso de que caracter se encuentre en la cadena hace esto:
             if (indice != -1) {
                 int indiceCodificado = (indice + desplazamiento) % ABECEDARIO.length();
+                //con append agregamos al resultado el caracter codificado, es decir con el desplazamiento aplicado
                 resultado.append(ABECEDARIO.charAt(indiceCodificado));
             } else {
+                //esto es lo que se puede mejorar, si ingreso cualquier caracter que no esté en ABECEDARIO mantiene el mismo dentro de la cadena, se podría aplicar una validación pero no se pide en la consigna.
                 resultado.append(caracter);
             }
         }
@@ -36,10 +42,10 @@ public class Codificador {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Ingrese una cadena: ");
+            System.out.print("Ingrese una palabra u oración: ");
             String cadena = scanner.nextLine();
 
-            System.out.print("Ingrese un numero de desplazamiento: ");
+            System.out.print("Ingrese un numero (entero positivo) de desplazamiento para aplicar la codificación: ");
             int desplazamiento = scanner.nextInt();
 
             String cadenaCodificada = Codificador.codificar(cadena, desplazamiento);
