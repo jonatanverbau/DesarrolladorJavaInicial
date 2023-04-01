ce02e52caedd7e49d8f877ff89fed6f8d59f3dfd
Jonatan <jonatanverbau@gmail.com>
2023-04-01 19:46:59 -0300

    mejora código de test

--- clase6/test/testCalculadora.java
+++ clase6/test/testCalculadora.java
@@ -6,34 +6,28 @@ import org.junit.*;
 
 import clase6.src.Calculadora;
 public class testCalculadora {
-
+  Calculadora c;
+  @Before
+  public void iniciarObjetoCalc(){
+  c= new Calculadora();
+  }
   @Test
   public void test1() {
-    
-    Calculadora c= new Calculadora();
-    
     assertEquals(240,c.multiplicar(80,3),0);
-    
   }
 
   @Test
   public void test2() {
-    Calculadora c= new Calculadora();
-    
     assertEquals(110,c.dividir(c.sumar(150,180),3),0);
   }
 
   @Test
   public void test3() {
-    Calculadora c= new Calculadora();
-    
     assertNotEquals(605,c.multiplicar(c.restar(90,50),15),0);
   }
 
   @Test
   public void test4() {
-    Calculadora c= new Calculadora();
-    
     assertNotEquals(2700,c.multiplicar(c.sumar(70,40),25),0);
   }
 }
