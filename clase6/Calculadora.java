793746ac0590846e03736db3fcd99edc9fb8c050
Jonatan <jonatanverbau@gmail.com>
2023-04-01 17:31:32 -0300

    clase 6

---
+++ clase6/Calculadora.java
@@ -0,0 +1,23 @@
+package clase6;
+
+public class Calculadora {
+
+  public double sumar(double unNumero, double otroNumero) {
+    return unNumero + otroNumero;
+  }
+
+  public double restar(double unNumero, double otroNumero) {
+    return unNumero - otroNumero;
+  }
+
+  public double multiplicar(double unNumero, double otroNumero) {
+    return unNumero * otroNumero;
+  }
+
+  public double dividir(double unNumero, double otroNumero) {
+    if (otroNumero == 0) {
+      throw new IllegalArgumentException("No se puede dividir por cero");
+    }
+    return unNumero / otroNumero;
+  }
+}
